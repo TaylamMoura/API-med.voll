5cf6b14cbdee02d37aeacc4604fcb56e92a7b6ed
Taylam Moura <tay.moura@hotmail.com>
2025-01-10 19:53:49 -0300

    Mover para domain e alterar metodos de void para ResponseEntity

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,13 +2,15 @@ package med.voll.api.controller;
 
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.*;
+import med.voll.api.domain.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 
 @RestController     //o java sabe que essa classe é um controller
@@ -21,29 +23,45 @@ public class MedicoController {
 
     @PostMapping
     @Transactional              //como vai fazer um insert no bd, precisa ter uma transação ativa com bd
-    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){ //o spring sabe que o parametro dados do metodo cadastrar, é para ser puxado do corpo da requisição.
-        repository.save(new Medico(dados));
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroMedico dados, UriComponentsBuilder uriBuilder){ //o spring sabe que o parametro dados do metodo cadastrar, é para ser puxado do corpo da requisição.
+        var medico = new Medico(dados);
+        repository.save(medico);
+        var uri = uriBuilder.path("/medicos/{id}").buildAndExpand(medico.getId()).toUri();
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoMedico(medico)); //-->cria um objeto ResponseEntity
     }
+    //UriComponentsBuilder = cria a url http://localhost:8080, ai so colocar o complemento
 
     @GetMapping
-    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, page = 0, sort = {"nome"}) Pageable paginacao){
-         return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+    public ResponseEntity<Page<DadosListagemMedico>> listar(@PageableDefault(size = 10, page = 0, sort = {"nome"}) Pageable paginacao){
+          var page = repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+          return ResponseEntity.ok(page);
     }
 
     @PutMapping
     @Transactional
-    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
+    public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
         var medico = repository.getReferenceById(dados.id());
         medico.atualizarInformacoes(dados);
+        
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
     }
 
     //EXCLUSÃO LÓGICA -- o marca como inativo, não o exclui de fato.
     @DeleteMapping("/{id}")
     @Transactional
-    public void excluir(@PathVariable Long id){
+    public ResponseEntity excluir(@PathVariable Long id){
         var medico = repository.getReferenceById(id);
         medico.excluir();
+        return ResponseEntity.noContent().build();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id){
+
+        var medico = repository.getReferenceById(id);
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
+    }
+
 
 //    @DeleteMapping("/{id}")
 //    @Transactional
