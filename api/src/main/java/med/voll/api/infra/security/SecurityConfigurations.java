ac03e4b71ca8163408e234d6347b1aa4d9ed5cfc
Taylam Moura <tay.moura@hotmail.com>
2025-01-10 19:55:40 -0300

    Criar classe de segurança

---
+++ api/src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -0,0 +1,22 @@
+package med.voll.api.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+
+    @Bean //expoe o retorno do metodo - o metodo devolve um objeto do tipo securityFilterChain (o bean diz isso ao spring)
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws  Exception{
+        return  http.csrf().disable()   //desabilita a proteção contra ataques do tipo CSRF (o token já é uma proteção)
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)  //determina que seja um stateless por ser uma API REST
+                .and().build();
+    }
+
+
+}
