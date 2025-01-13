44a861ca2614d8da8ba09e8e1331d9fab204c678
Taylam Moura <tay.moura@hotmail.com>
2025-01-13 16:23:09 -0300

    Adicionar metodo getSubject

--- api/src/main/java/med/voll/api/infra/security/TokenService.java
+++ api/src/main/java/med/voll/api/infra/security/TokenService.java
@@ -3,14 +3,10 @@ package med.voll.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.voll.api.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
-import java.security.interfaces.RSAPublicKey;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
 
 @Service
 public class TokenService {
@@ -20,21 +16,34 @@ public class TokenService {
 
     public String gerarToken(Usuario usuario) {  //é um String pq vai retornar um token
         try {
-            RSAPublicKey rsaPublicKey;
             var algoritmo = Algorithm.HMAC256(secret);
             return JWT.create()
                     .withIssuer("API Voll.med") //info que vai dentro do token, de quem está gerando o token.
                     .withSubject(usuario.getLogin())  //info de quem é o usuario relacionado ao token
-                    .withExpiresAt(dataExpiracao())
+//                    .withExpiresAt(dataExpiracao())
                     .sign(algoritmo);
         } catch (JWTCreationException exception) {
             throw new RuntimeException("Erro ao gerar token jwt", exception);
         }
+    } //este metodo válida o token, se tiver invalido lança um exception, se tiver valido ele pega o subject de dentro do token
+
+    public String getSubject(String tokenJWT){ //aqui passa um token, e o metodo verifica se o token é valido e devolve o usuario que esta armazenado dentro do token
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API Voll.med")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("TokenJWT inválido ou expirado!");
+        }
+
     }
 
-    private Instant dataExpiracao() {
-        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
-    }
+//    private Instant dataExpiracao() {
+//        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+//    }
 }
 
 //precisa ficar dentro de um try/catch pq essa classe lança exceções
