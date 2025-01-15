5515ff2e9823d30d625a2a47c22df64ca1e5c5d3
Taylam Moura <tay.moura@hotmail.com>
2025-01-15 20:15:41 -0300

    Criar Validador (classe)

---
+++ api/src/main/java/med/voll/api/domain/consultas/validacoes/agendamento/ValidadorPacienteAtivo.java
@@ -0,0 +1,25 @@
+package med.voll.api.domain.consultas.validacoes.agendamento;
+
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consultas.agendamento.DadosAgendamentoConsulta;
+import med.voll.api.domain.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ValidadorPacienteAtivo implements ValidadorAgendamentoDeConsulta {
+
+    @Autowired
+    private PacienteRepository repository;
+
+    public void validar(DadosAgendamentoConsulta dados){
+        var pacienteEstaAtivo = repository.findAtivoById(dados.idPaciente());
+
+        if (!pacienteEstaAtivo){
+            throw new ValidacaoException("consulta não pode ser marcada por paciente excluído");
+        }
+
+    }
+}
+
+
