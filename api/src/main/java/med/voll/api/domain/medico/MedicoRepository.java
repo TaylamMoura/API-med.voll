e58b265c877af81f612ce49e6764c9ea3864a12d
Taylam Moura <tay.moura@hotmail.com>
2025-01-16 19:06:22 -0300

    Excluir imports inutilizados

--- api/src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ api/src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -1,15 +1,9 @@
 package med.voll.api.domain.medico;
 
-import jakarta.persistence.QueryHint;
-import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.NotNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.QueryHints;
-import org.springframework.data.repository.query.Param;
-
 
 import java.time.LocalDateTime;
 
